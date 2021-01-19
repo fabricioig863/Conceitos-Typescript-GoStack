1df7f29cee2f20dec38b889e7604e556b7545c32
Fabricio <fabricioig863@gmail.com>
2021-01-19 13:57:11 -0300

    Arquivo index.ts alterado

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.send('Hello world')
+    return response.json({ message: 'Hello gostack' })
 })
 
 
