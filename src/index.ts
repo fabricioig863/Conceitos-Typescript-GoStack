cca3fbdfcf890e0e75a8e199616d0baa99956d38
Fabricio <fabricioig863@gmail.com>
2021-01-19 13:52:30 -0300

    Arquivo index.ts alterado mudança na linha 6

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello world'})
+    return response.send('Hello world')
 })
+
 
 app.listen(3333);
